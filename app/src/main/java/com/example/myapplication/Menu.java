4928d191f658bc7f574f95ffbb3afc6e4359753b
swan10 <swarnendu10.sg@yahoo.com>
2020-05-19 13:19:45 +0530

    Added custom SMS, gps function and completed whole dashboard

--- app/src/main/java/com/example/myapplication/Menu.java
+++ app/src/main/java/com/example/myapplication/Menu.java
@@ -1,32 +1,28 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.Manifest;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
+
+import static android.Manifest.permission;
 
 public class Menu extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
@@ -36,8 +32,10 @@ public class Menu extends AppCompatActivity implements PopupMenu.OnMenuItemClick
     public static final String[] s={"P1","P2","P3","P4","P5","P6","P7","PC1","PC2","PC3"};
     private static final int REQUEST_CALL = 1;
     private static final int REQUEST_SEND_SMS = 1;
+
     ImageView im1,im2;
     TextView g1im1,g1im2,g1im3,g1tv1,g1tv2,g1tv3;
+
 
 
     @Override
@@ -79,21 +77,21 @@ public class Menu extends AppCompatActivity implements PopupMenu.OnMenuItemClick
             TextView g1tv3=findViewById(R.id.g1tv3);
             //g1im1.setText(cursor.getString(cursor.getColumnIndexOrThrow("P1")));
             if(cursor.getString(cursor.getColumnIndexOrThrow("P1")).length()==0){
-
+                return;
             }else{
                 g1im1.setText(cursor.getString(cursor.getColumnIndexOrThrow("P1")));
             }
             g1tv1.setText(cursor.getString(cursor.getColumnIndexOrThrow("PC1")));
             //g1im2.setText(cursor.getString(cursor.getColumnIndexOrThrow("P2")));
             if(cursor.getString(cursor.getColumnIndexOrThrow("P2")).length()==0){
-
+                return;
             }else{
                 g1im2.setText(cursor.getString(cursor.getColumnIndexOrThrow("P2")));
             }
             g1tv2.setText(cursor.getString(cursor.getColumnIndexOrThrow("PC2")));
             //g1im3.setText(cursor.getString(cursor.getColumnIndexOrThrow("P3")));
             if(cursor.getString(cursor.getColumnIndexOrThrow("P3")).length()==0){
-
+                return;
             }else{
                 g1im3.setText(cursor.getString(cursor.getColumnIndexOrThrow("P3")));
             }
@@ -204,6 +202,8 @@ public class Menu extends AppCompatActivity implements PopupMenu.OnMenuItemClick
             }
         });
 
+
+
         //bottom navigation
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
@@ -241,9 +241,9 @@ public class Menu extends AppCompatActivity implements PopupMenu.OnMenuItemClick
     //phone call
     private void makePhCall(String res) {
         if (ContextCompat.checkSelfPermission(Menu.this,
-                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(Menu.this,
-                    new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+                    new String[]{permission.CALL_PHONE}, REQUEST_CALL);
         } else {
             String dial = "tel:" + res;
             startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
@@ -282,15 +282,13 @@ public class Menu extends AppCompatActivity implements PopupMenu.OnMenuItemClick
                 }
                 return true;
             case R.id.c12:
+            case R.id.c13:
                 String res12 = fDb.getData(s[7]);
                 if(res12.length()==0){
                     aletDailog();
                 }else{
-                    checkSmsPermission();
+                    checkSmsPermission(res12);
                 }
-                return true;
-            case R.id.c13:
-                Toast.makeText(this, "Item c13 clicked", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.c21:
                 String res2 = fDb.getData(s[8]);
@@ -301,15 +299,13 @@ public class Menu extends AppCompatActivity implements PopupMenu.OnMenuItemClick
                 }
                 return true;
             case R.id.c22:
+            case R.id.c23:
                 String res22 = fDb.getData(s[8]);
                 if(res22.length()==0){
                     aletDailog();
                 }else{
-                    checkSmsPermission();
+                    checkSmsPermission(res22);
                 }
-                return true;
-            case R.id.c23:
-                Toast.makeText(this, "Item c23 clicked", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.c31:
                 String res3 = fDb.getData(s[9]);
@@ -320,15 +316,13 @@ public class Menu extends AppCompatActivity implements PopupMenu.OnMenuItemClick
                 }
                 return true;
             case R.id.c32:
+            case R.id.c33:
                 String res32 = fDb.getData(s[9]);
                 if(res32.length()==0){
                     aletDailog();
                 }else{
-                    checkSmsPermission();
+                    checkSmsPermission(res32);
                 }
-                return true;
-            case R.id.c33:
-                Toast.makeText(this, "Item c33 clicked", Toast.LENGTH_SHORT).show();
                 return true;
             default:
                 return false;
@@ -357,14 +351,21 @@ public class Menu extends AppCompatActivity implements PopupMenu.OnMenuItemClick
 
 
     //check for sms permission
-    private void checkSmsPermission() {
+    private void checkSmsPermission(String res) {
         if (ContextCompat.checkSelfPermission(Menu.this,
-                Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(Menu.this,
-                    new String[]{Manifest.permission.SEND_SMS}, REQUEST_SEND_SMS);
+                    new String[]{permission.SEND_SMS}, REQUEST_SEND_SMS);
         } else {
-            Intent intent=new Intent(Menu.this,custom_sms.class);
-            startActivity(intent);
+            makeSms(res);
         }
+    }
+
+    //sending sms to intent
+
+    public void makeSms(String s){
+        Intent intent=new Intent(Menu.this,custom_sms.class);
+        intent.putExtra("no",s);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myapplication/custom_sms.java
+++ app/src/main/java/com/example/myapplication/custom_sms.java
@@ -1,8 +1,15 @@
 package com.example.myapplication;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +17,17 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class custom_sms extends AppCompatActivity {
     EditText cs2,cs3;
-    Button cs4,cs5;
+    Button cs4,cs5,csgps;
+    private LocationManager locationManager;
+    private LocationListener listener;
+    private static final int REQUEST_GPS = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +37,14 @@ public class custom_sms extends AppCompatActivity {
         cs3=findViewById(R.id.cs3);
         cs4=findViewById(R.id.cs4);
         cs5=findViewById(R.id.cs5);
+        csgps=findViewById(R.id.csgps);
+
+        //fetch previous details
+        Intent intent=getIntent();
+        String no=intent.getStringExtra("no");
+        cs2.setText(no);
+
+        //onclick actions
 
         cs4.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +54,16 @@ public class custom_sms extends AppCompatActivity {
             }
         });
 
+        //add gps onclick
+        csgps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkGpsPermission();
+            }
+        });
+
+        //onclick send sms
+
         cs5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,20 +71,39 @@ public class custom_sms extends AppCompatActivity {
                     aletDailog();
                 }
                 else{
-                    try{
-                        SmsManager smgr = SmsManager.getDefault();
-                        smgr.sendTextMessage(cs2.getText().toString().trim(),null,cs3.getText().toString(),null,null);
-
-                        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "SMS Sent Successfully", Snackbar.LENGTH_LONG);
-                        snackbar.show();
-                    }
-                    catch (Exception e){
-                        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "SMS Failed to Send, Please try again", Snackbar.LENGTH_LONG);
-                        snackbar.show();
-                    }
+                    String num=cs2.getText().toString().trim();
+                    String body=cs3.getText().toString();
+                    makeSms(num,body);
                 }
             }
         });
+
+        //fetch location
+        //location
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                String body = cs3.getText().toString()+"\nhttp://maps.google.com/maps?saddr=" + location.getLatitude()+","+location.getLongitude();
+                cs3.setText(body);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
     }
     //back button
     public void onBackPressed(){
@@ -74,5 +123,39 @@ public class custom_sms extends AppCompatActivity {
                     }
                 }).show();
 
+    }
+
+
+    //check gps permission
+    private void checkGpsPermission(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
+                        , REQUEST_GPS);
+            }
+            return;
+        }
+        try{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
+        }
+        catch (Exception e){
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
+        }
+    }
+
+    //send sms
+    public void makeSms(String num,String body){
+        try{
+            SmsManager smgr = SmsManager.getDefault();
+            smgr.sendTextMessage(num,null,body,null,null);
+
+            Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "SMS Sent Successfully", Snackbar.LENGTH_LONG);
+            snackbar.show();
+        }
+        catch (Exception e){
+            Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "SMS Failed to Send, Please try again", Snackbar.LENGTH_LONG);
+            snackbar.show();
+        }
     }
 }
